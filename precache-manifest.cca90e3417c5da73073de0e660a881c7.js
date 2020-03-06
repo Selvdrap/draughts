e630761954d307cb81aac9c21b91a5eb015c596f
Gregory <gregory-starodubcev@yandex.ru>
2020-03-06 03:52:56 +0300

    Updates

---
+++ precache-manifest.cca90e3417c5da73073de0e660a881c7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23fc0d22e83084ce50997e4630b339ae",
+    "url": "/Draughts/index.html"
+  },
+  {
+    "revision": "0754d73492dd63377c0d",
+    "url": "/Draughts/static/js/2.4a5f2fd2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Draughts/static/js/2.4a5f2fd2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8a749d827f8b4bebe5d",
+    "url": "/Draughts/static/js/main.b50f1a54.chunk.js"
+  },
+  {
+    "revision": "21f5a40cd26221dfc638",
+    "url": "/Draughts/static/js/runtime-main.61e36329.js"
+  },
+  {
+    "revision": "bed1e2407dcba8bc5f97accab772d031",
+    "url": "/Draughts/static/media/blue-button.bed1e240.jpg"
+  },
+  {
+    "revision": "a2029ccb55bf49c8a453d09677014127",
+    "url": "/Draughts/static/media/blue-piece.a2029ccb.jpg"
+  },
+  {
+    "revision": "959e0ef3146e685013ade03720ddfb75",
+    "url": "/Draughts/static/media/crown.959e0ef3.svg"
+  },
+  {
+    "revision": "ffb3499c399274faf4b3e21753f8cdbb",
+    "url": "/Draughts/static/media/footprint.ffb3499c.svg"
+  },
+  {
+    "revision": "42cee0df829f4f88fb0e6be651f172df",
+    "url": "/Draughts/static/media/hinge.42cee0df.svg"
+  },
+  {
+    "revision": "9ec9b4a936584d9a18f1e3f03f0ee3f4",
+    "url": "/Draughts/static/media/lock.9ec9b4a9.svg"
+  },
+  {
+    "revision": "48aa81e2781280dc9827e23cb5073b76",
+    "url": "/Draughts/static/media/red-button.48aa81e2.jpg"
+  },
+  {
+    "revision": "ae98a9f5235f676a96e40714995fd648",
+    "url": "/Draughts/static/media/red-piece.ae98a9f5.jpg"
+  },
+  {
+    "revision": "f2952b78193c9a00feb9532731fb9b27",
+    "url": "/Draughts/static/media/sword.f2952b78.svg"
+  },
+  {
+    "revision": "3e60da169b0ca75ad98be745c42ab2c5",
+    "url": "/Draughts/static/media/timer.3e60da16.svg"
+  },
+  {
+    "revision": "f123d511caa1a05d8414170bd0ddeb2d",
+    "url": "/Draughts/static/media/user.f123d511.svg"
+  }
+]);
